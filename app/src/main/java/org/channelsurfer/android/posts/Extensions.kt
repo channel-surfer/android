17623c0d29221d8ed5506385beb2a3fa8e7ba6f5
jupl <juan.placencia.512@gmail.com>
2015-10-11 15:19:47 -0500

    Refactor

--- app/src/main/java/org/channelsurfer/android/posts/Extensions.kt
+++ app/src/main/java/org/channelsurfer/android/posts/Extensions.kt
@@ -1,5 +1,6 @@
 package org.channelsurfer.android.posts
 
+import android.content.Context
 import com.github.salomonbrys.kotson.fromJson
 import com.github.salomonbrys.kotson.simpleDeserialize
 import com.google.gson.Gson
@@ -16,9 +17,21 @@ private val fetchGson: Gson = createGson {
     }
 }
 
-private val Board.postsUrl : String get() = "https://8ch.net/$name/catalog.json"
+val Board.postsUrl: String get() = "https://8ch.net/$name/catalog.json"
 
-fun Network.fetchPosts(board: Board, callback: (List<Post>?, Exception?) -> Unit) {
+// TODO Remove/change later as this is temporary caching
+var Context.cachedPosts: List<Post>
+    get() {
+        val string = getSharedPreferences("threads", Context.MODE_PRIVATE).getString("threads", "[]")
+        return gson.fromJson<List<Post>>(string)!!
+    }
+    set(value) {
+        val editor = getSharedPreferences("threads", Context.MODE_PRIVATE).edit()
+        editor.putString("threads", gson.toJson(value).toString())
+        editor.commit()
+    }
+
+fun Network.getPosts(board: Board, callback: (List<Post>?, Exception?) -> Unit) {
     string(url=board.postsUrl) { response, error ->
         callback(if(error == null && response != null) fetchGson.fromJson<List<Post>>(response) else null, error)
     }
--- app/src/main/java/org/channelsurfer/android/posts/PostsAdapter.kt
+++ app/src/main/java/org/channelsurfer/android/posts/PostsAdapter.kt
@@ -2,10 +2,8 @@ package org.channelsurfer.android.posts
 
 import android.content.Context
 import android.view.ViewGroup
-import com.github.salomonbrys.kotson.fromJson
 import org.channelsurfer.android.base.RecyclerAdapter
 import org.channelsurfer.android.base.network
-import org.channelsurfer.android.base.gson
 import org.channelsurfer.android.boards.Board
 import org.jetbrains.anko.async
 import org.jetbrains.anko.uiThread
@@ -17,25 +15,16 @@ public class PostsAdapter(
         private val board = Board("tech", "Technology")
     }
 
-    init {
-        // TODO Remove/change later as this is temporary caching
-        data = run {
-            val string = context.getSharedPreferences("threads", Context.MODE_PRIVATE).getString("threads", "[]")
-            gson.fromJson<List<Post>>(string)!!
-        }
-    }
+    init { data = context.cachedPosts }
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int) = PostsItemView.Holder(context) { onClick(it) }
 
     override fun update(callback: (Exception?) -> Unit) {
         async {
-            context.network.fetchPosts(board) { posts, error ->
+            context.network.getPosts(board) { posts, error ->
                 uiThread {
-                    // TODO Remove/change later as this is temporary caching
                     if(posts != null) {
-                        val editor = context.getSharedPreferences("threads", Context.MODE_PRIVATE).edit()
-                        editor.putString("threads", gson.toJson(posts).toString())
-                        editor.commit()
+                        context.cachedPosts = posts
                         data = posts
                     }
                     callback(error)
